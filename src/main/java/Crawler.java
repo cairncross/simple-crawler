87eb5af05393f9729370286a16fcadfafca756e1
Euan Cairncross <euan@cairncross.io>
2018-11-23 17:40:15 +0000

    adds TreeNode to represent sitemap hierarchy

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -3,52 +3,59 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 public class Crawler {
 
-    private Queue<String> queue = new LinkedList<>();
+    private Queue<TreeNode> queue = new LinkedList<>();
     private Set<String> visited = new HashSet<>();
+    private Set<String> seen = new HashSet<>();
     private String domain;
+    private TreeNode root;
 
     public Crawler(String startUrl, String domain) throws IllegalArgumentException {
         this.domain = domain;
         if (!isWithinDomain(startUrl)) {
             throw new IllegalArgumentException("startUrl " + startUrl + " is not within domain " + domain);
         }
-        queue.add(startUrl);
+        root = new TreeNode(startUrl);
+        queue.add(root);
     }
 
 
     public void run() {
         System.out.println("running...");
-        String url;
+        TreeNode node;
         while(!queue.isEmpty()) {
-            url = queue.remove();
+            node = queue.remove();
+            String url = node.getUrl();
             if (!visited.contains(url)) {
-                crawl(url);
+                node.setChildren(crawl(url));
             }
         }
         System.out.println("finished");
     }
 
-    public void crawl(String url) {
+    private List<TreeNode> crawl(String url) {
         System.out.println("crawling: " + url);
         visited.add(url);
+        List<TreeNode> children = new ArrayList<>();
         Elements links;
         try {
             links = Jsoup.connect(url).get().select("a[href]");
             for (Element link: links) {
-                if (isWithinDomain(link.absUrl("href"))) {
-                    queue.add(link.absUrl("href"));
+                String childUrl = link.absUrl("href");
+                if (isWithinDomain(childUrl) && !seen.contains(childUrl)) {
+                    seen.add(childUrl);
+                    TreeNode child = new TreeNode(childUrl);
+                    children.add(child);
+                    queue.add(child);
                 }
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+        return children;
     }
 
     public boolean isWithinDomain(String url) {
---
+++ src/main/java/TreeNode.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+public class TreeNode {
+
+    private String url;
+    private List<TreeNode> children;
+
+    public TreeNode(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<TreeNode> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<TreeNode> children) {
+        this.children = children;
+    }
+}
